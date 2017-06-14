824e6302a649217005f74a3a92cfb5ae308400c9
Chenshiwen <cswzmg@live.com>
2017-06-14 12:41:18 +0800

    灯光使用

--- SceneKit-03/ViewController.m
+++ SceneKit-03/ViewController.m
@@ -7,8 +7,11 @@
 //
 
 #import "ViewController.h"
+#import <SceneKit/SceneKit.h>
 
 @interface ViewController ()
+@property (nonatomic,strong) SCNView *scnView;
+
 
 @end
 
@@ -16,9 +19,55 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    SCNScene *scene = [SCNScene scene];
+    self.scnView.scene =  scene;
+    
+    //创建球
+    SCNSphere *sphere = [SCNSphere sphereWithRadius:0.5];
+    sphere.firstMaterial.diffuse.contents = [UIColor yellowColor];
+    SCNNode *sphereNode = [SCNNode node ];
+    sphereNode.geometry = sphere;
+    
+    [scene.rootNode addChildNode:sphereNode];
+    
+    
+    
+//    灯光
+    SCNLight *light = [SCNLight light];
+//    灯光颜色
+    light.color = [UIColor whiteColor];
+//    设置灯光类型
+    light.type = SCNLightTypeOmni;
+//    内夹角
+//    light.spotInnerAngle = 20;
+////    是否捕捉阴影
+//    light.castsShadow = YES;
+
+////    灯光能照射最远单位
+    light.zFar = 10;
+    SCNNode *lightNode = [SCNNode node];
+    lightNode.light = light;
+//    位置
+    lightNode.position = SCNVector3Make(1, 0.5, 0.7);
+    [sphereNode addChildNode:lightNode];
+
+    
+    [self.view addSubview:self.scnView];
+    
+    
+    
 }
 
+-(SCNView *)scnView{
+    if (!_scnView) {
+        _scnView = [[SCNView alloc] initWithFrame:CGRectMake(0, 0, 300, 300)];
+        _scnView.center = self.view.center;
+        _scnView.backgroundColor = [UIColor redColor];
+//        开启交互
+        _scnView.allowsCameraControl = YES;
+    }
+    return _scnView;
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
